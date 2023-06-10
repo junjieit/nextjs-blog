282256101d85303e383d044bf9223a13ed299e5e
junjie <wangjunjie0817@foxmail.com>
2023-06-10 16:35:29 +0800

    fix build error

--- app/article/new/[id]/page.tsx
+++ app/article/new/[id]/page.tsx
@@ -1,26 +1,26 @@
-import { getArticleDetail, getSortedPostsData } from "@/lib/posts";
-import ArticleEditor from "../ArticleEditor";
+import { getArticleDetail, getSortedPostsData } from '@/lib/posts'
+import ArticleEditor from '../ArticleEditor'
 
 interface Props {
   params: {
-    id: string;
-  };
+    id: string
+  }
 }
 
 export default async function ArticleEdit({ params }: Props) {
-  const { id } = params;
-  const markdown = await getArticleDetail(decodeURI(id));
+  const { id } = params
+  const markdown = await getArticleDetail(decodeURI(id))
   return (
     <div>
-      <ArticleEditor markdown={markdown} />
+      <ArticleEditor />
     </div>
-  );
+  )
 }
 
 export async function generateStaticParams() {
-  const posts = await getSortedPostsData();
+  const posts = await getSortedPostsData()
 
   return posts.map((post) => ({
     id: post.id,
-  }));
+  }))
 }
